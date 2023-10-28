36ae6d381f9a6443ff0f6ddf2a29a50187d506e3
HarryHeres <personal@janheres.eu>
2023-10-28 12:06:48 +0200

    Updating UI

--- SlicerBoneMorphing/src/widget/SlicerBoneMorphingWidget.py
+++ SlicerBoneMorphing/src/widget/SlicerBoneMorphingWidget.py
@@ -1,9 +1,13 @@
-from src.logic.Constants import EXIT_OK
+import src.logic.Constants 
 import ctk
 import slicer
 from slicer.ScriptedLoadableModule import ScriptedLoadableModuleWidget
 from src.logic.SlicerBoneMorphingLogic import SlicerBoneMorphingLogic
+from enum import Enum
 
+class BCPDMode(Enum):
+    STANDARD = "bcpd"
+    GEODESIC = "gbcpd"
 
 class SlicerBoneMorphingWidget(ScriptedLoadableModuleWidget):
   """Uses ScriptedLoadableModuleWidget base class, available at:
@@ -13,6 +17,7 @@ class SlicerBoneMorphingWidget(ScriptedLoadableModuleWidget):
   def __init__(self, parent):
     """Called when the application opens the module the first time and the widget is initialized."""
     ScriptedLoadableModuleWidget.__init__(self, parent)
+    self.bcpd_options = {}
 
 
   def setup(self):
@@ -29,9 +34,25 @@ class SlicerBoneMorphingWidget(ScriptedLoadableModuleWidget):
     self.ui.targetNodeSelectionBox.setMRMLScene(slicer.mrmlScene)
     self.ui.generateModelButton.clicked.connect(self.generate_model)
 
+    bcpd_mode_combo_box = self.ui.bcpdModeComboBox
+    for mode in list(BCPDMode):
+        bcpd_mode_combo_box.addItem(mode.name, mode)
+    bcpd_mode_combo_box.setCurrentIndex(0)
+
+  def set_default_parameters(self):
+      print("Test")
+    
+
   def generate_model(self):
-      bcpd_b = self.ui.bcpdDeformationVectorLengthSpinBox.value()
-      print("BCPD B: " + str(bcpd_b))
+    mode = self.ui.bcpdModeComboBox.currentData
+    if(mode == None):
+        print("Please, select a proper BCPD registration mode")
+    self.bcpd_options["mode"] = mode
+
+    dvl = self.ui.bcpdDeformationVectorLengthSpinBox.value
+    self.bcpd_options["b"] = dvl
+
+    print("TEST: " + str(mode.value))
 
       # code, vtk_polydata = self.logic.generate_model(self.ui.sourceNodeSelectionBox.currentNode(), self.ui.targetNodeSelectionBox.currentNode())
 
@@ -39,6 +60,8 @@ class SlicerBoneMorphingWidget(ScriptedLoadableModuleWidget):
       #   model_node = slicer.mrmlScene.AddNewNodeByClass('vtkMRMLModelNode', 'BCPD result')
       #   model_node.SetAndObservePolyData(vtk_polydata)
       #   model_node.CreateDefaultDisplayNodes()  
+
+    
 
 
             
